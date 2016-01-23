212058ae615997a9ec94d41326151e4674dfec1b
Steve Purcell <steve@sanityinc.com>
2016-01-23 15:14:16 +1300

    Valid and invalid sample data

---
+++ invalid.rb
@@ -0,0 +1,399 @@
+foo0 = bar
+
+foo1 = bar
+
+foo2 = bar
+
+foo3 = bar
+
+foo4 = bar
+
+foo5 = bar
+
+foo6 = bar
+
+foo7 = bar
+
+foo8 = bar
+
+foo9 = bar
+
+foo10 = bar
+
+foo11 = bar
+
+foo12 = bar
+
+foo13 = bar
+
+foo14 = bar
+
+foo15 = bar
+
+foo16 = bar
+
+foo17 = bar
+
+foo18 = bar
+
+foo19 = bar
+
+foo20 = bar
+
+foo21 = bar
+
+foo22 = bar
+
+foo23 = bar
+
+foo24 = bar
+
+foo25 = bar
+
+foo26 = bar
+
+foo27 = bar
+
+foo28 = bar
+
+foo29 = bar
+
+foo30 = bar
+
+foo31 = bar
+
+foo32 = bar
+
+foo33 = bar
+
+foo34 = bar
+
+foo35 = bar
+
+foo36 = bar
+
+foo37 = bar
+
+foo38 = bar
+
+foo39 = bar
+
+foo40 = bar
+
+foo41 = bar
+
+foo42 = bar
+
+foo43 = bar
+
+foo44 = bar
+
+foo45 = bar
+
+foo46 = bar
+
+foo47 = bar
+
+foo48 = bar
+
+foo49 = bar
+
+foo50 = bar
+
+foo51 = bar
+
+foo52 = bar
+
+foo53 = bar
+
+foo54 = bar
+
+foo55 = bar
+
+foo56 = bar
+
+foo57 = bar
+
+foo58 = bar
+
+foo59 = bar
+
+foo60 = bar
+
+foo61 = bar
+
+foo62 = bar
+
+foo63 = bar
+
+foo64 = bar
+
+foo65 = bar
+
+foo66 = bar
+
+foo67 = bar
+
+foo68 = bar
+
+foo69 = bar
+
+foo70 = bar
+
+foo71 = bar
+
+foo72 = bar
+
+foo73 = bar
+
+foo74 = bar
+
+foo75 = bar
+
+foo76 = bar
+
+foo77 = bar
+
+foo78 = bar
+
+foo79 = bar
+
+foo80 = bar
+
+foo81 = bar
+
+foo82 = bar
+
+foo83 = bar
+
+foo84 = bar
+
+foo85 = bar
+
+foo86 = bar
+
+foo87 = bar
+
+foo88 = bar
+
+foo89 = bar
+
+foo90 = bar
+
+foo91 = bar
+
+foo92 = bar
+
+foo93 = bar
+
+foo94 = bar
+
+foo95 = bar
+
+foo96 = bar
+
+foo97 = bar
+
+foo98 = bar
+
+foo99 = bar
+
+foo100 = bar
+
+foo101 = bar
+
+foo102 = bar
+
+foo103 = bar
+
+foo104 = bar
+
+foo105 = bar
+
+foo106 = bar
+
+foo107 = bar
+
+foo108 = bar
+
+foo109 = bar
+
+foo110 = bar
+
+foo111 = bar
+
+foo112 = bar
+
+foo113 = bar
+
+foo114 = bar
+
+foo115 = bar
+
+foo116 = bar
+
+foo117 = bar
+
+foo118 = bar
+
+foo119 = bar
+
+foo120 = bar
+
+foo121 = bar
+
+foo122 = bar
+
+foo123 = bar
+
+foo124 = bar
+
+foo125 = bar
+
+foo126 = bar
+
+foo127 = bar
+
+foo128 = bar
+
+foo129 = bar
+
+foo130 = bar
+
+foo131 = bar
+
+foo132 = bar
+
+foo133 = bar
+
+foo134 = bar
+
+foo135 = bar
+
+foo136 = bar
+
+foo137 = bar
+
+foo138 = bar
+
+foo139 = bar
+
+foo140 = bar
+
+foo141 = bar
+
+foo142 = bar
+
+foo143 = bar
+
+foo144 = bar
+
+foo145 = bar
+
+foo146 = bar
+
+foo147 = bar
+
+foo148 = bar
+
+foo149 = bar
+
+foo150 = bar
+
+foo151 = bar
+
+foo152 = bar
+
+foo153 = bar
+
+foo154 = bar
+
+foo155 = bar
+
+foo156 = bar
+
+foo157 = bar
+
+foo158 = bar
+
+foo159 = bar
+
+foo160 = bar
+
+foo161 = bar
+
+foo162 = bar
+
+foo163 = bar
+
+foo164 = bar
+
+foo165 = bar
+
+foo166 = bar
+
+foo167 = bar
+
+foo168 = bar
+
+foo169 = bar
+
+foo170 = bar
+
+foo171 = bar
+
+foo172 = bar
+
+foo173 = bar
+
+foo174 = bar
+
+foo175 = bar
+
+foo176 = bar
+
+foo177 = bar
+
+foo178 = bar
+
+foo179 = bar
+
+foo180 = bar
+
+foo181 = bar
+
+foo182 = bar
+
+foo183 = bar
+
+foo184 = bar
+
+foo185 = bar
+
+foo186 = bar
+
+foo187 = bar
+
+foo188 = bar
+
+foo189 = bar
+
+foo190 = bar
+
+foo191 = bar
+
+foo192 = bar
+
+foo193 = bar
+
+foo194 = bar
+
+foo195 = bar
+
+foo196 = bar
+
+foo197 = bar
+
+foo198 = bar
+
+foo199 = bar
